638b60dd7842cf56aa5bbc3319f93c95b58c95a6
gohitx <gohit21@gmail.com>
2026-02-08 19:09:23 -0500

    ⚙️ Refactor: Organize icons into dedicated active and inactive states for the bottom bar, replacing previous generic icon files.

--- src/components/icons/bottom-bar/inactive/message.tsx
+++ src/components/icons/bottom-bar/inactive/message.tsx
@@ -8,9 +8,10 @@
   strokeWidth="2"
   strokeLinecap="round"
   strokeLinejoin="round"
-  className="icon icon-tabler icons-tabler-outline icon-tabler-user"
+  className="icon icon-tabler icons-tabler-outline icon-tabler-message-2"
 >
   <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-  <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
-  <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+  <path d="M8 9h8" />
+  <path d="M8 13h6" />
+  <path d="M9 18h-3a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v8a3 3 0 0 1 -3 3h-3l-3 3l-3 -3" />
 </svg>;
